6fe255d4505b49f35e426374d06bde65bf7fef19
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-03-20 20:45:03 -0400

    version 0.6 working on JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,9 +1,11 @@
-$(document).getready(function(){
+$(document).ready(function(){
 //Set up my Key:
-var APIKey = "647fbf122ebdb908d9e9758d0ce8bf5e"
+var APIKey = "beb12de68de22cd70503838ed01ed80c"
 //build URL:
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-"q=Bujumbura,Burundi&appid=" + APIKey;
+//Main City forecast (1day)
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=orlando&appid=" + APIKey;
+//5 day forecast
+var queryURL2 = "https://api.openweathermap.org/data/2.5/forecast?q=orlando&appid=" + APIKey;
 //Run Ajax:
 $.ajax({
     url: queryURL,
@@ -19,18 +21,26 @@ $.ajax({
 //grab my content to place in HTML
     
     
-    
-    
-    
-    
-    
-    
+    });
+
+//create a event listener for the button
+//create a function for the handler
+
+$.ajax({
+    url: queryURL2,
+    method: "GET"
+  })
+    //store data in response
+    .then(function(response) {
+// Log the queryURL
+      console.log(queryURL2);
+// Log the resulting object
+      console.log(response);
+
+//grab my content to place in HTML
     
     
     });
-
-
-
 
 
 
